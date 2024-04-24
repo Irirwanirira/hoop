98047115de8b051e4c46c33740e0c7b6ccd8ddff
23nosurrend <keynesbizimana@gmail.com>
2024-04-24 18:48:26 +0200

    add appwrite setup

---
+++ src/appwrite/Appwrite.ts
@@ -0,0 +1,12 @@
+import {Client, Account , Databases} from "react-native-appwrite"
+
+
+const client = new Client();
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('66267e925bd50045f82b');
+const databases = new Databases(client);
+
+const account = new Account(client)
+
+export { databases, account}
