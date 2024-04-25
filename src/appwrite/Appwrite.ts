b8fca92e4edf98328b3ea3466ea8fec804930a40
Keynes-Bizimana <102511280+23nosurrend@users.noreply.github.com>
2024-04-25 23:22:03 +0200

    feat(viewSingleParking) add functionality to render a single parking (#15)

--- src/appwrite/Appwrite.ts
+++ src/appwrite/Appwrite.ts
@@ -3,8 +3,13 @@ import {Client, Account , Databases} from "react-native-appwrite"
 
 const client = new Client();
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('66267e925bd50045f82b');
+    // .setEndpoint('https://cloud.appwrite.io/v1')
+    // .setProject('66267e925bd50045f82b');
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject("66267df155ecc331dbc8")
+    
+
+
 const databases = new Databases(client);
 
 const account = new Account(client)
